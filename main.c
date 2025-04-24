0243057bd4e5bee653347e88d7cfc7728d361b4d
cjonaspersson@gmail.com <cjonaspersson@gmail.com>
2025-04-24 17:24:11 +0200

    Add_name function added

--- main.c
+++ main.c
@@ -1,2 +1,48 @@
-//Write your code in this file
+#include <stdio.h>
 
+
+// adding names
+void add_names(){
+    printf("Add name & score: ");
+    char name[10];
+    int score[12];
+
+    scanf("%s", name);
+
+    for(int i = 0; i <=13; i++){
+        score[i] = scanf("%d");
+    }
+    printf("%s added\nScore: %d %d %d %d %d %d %d %d %d %d %d %d %d", name, score[0], score[1], score[2], score[3], score[4], score[5], score[6], score[7], score[8], score[9], score[10], score[11], score[12]);
+
+}
+
+//calculate average
+float calculate_average(int results[]){
+    float avg;
+    int sum;
+
+    for(int i = 0; i <= 13; i++){
+       sum += results[i];
+    }
+
+    avg = sum / 13.0;
+
+    return avg;
+}
+//calculate highest average
+
+//compare average
+float compare_average(float a, float b){
+
+}
+
+//change to capital first letter
+
+// print data
+
+int main(){
+    add_names();
+
+
+    return 0;
+}
